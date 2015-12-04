9647632c6e3e692b13503af6a1f01e94dcc99a2e
Thomas VICARIO <vicario.samoht@gmail.com>
2015-12-04 12:11:00 +0100

    Ajout du support du Cross-Origin

--- serveur_club.js
+++ serveur_club.js
@@ -2,6 +2,11 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var cors = require("cors");
+
+app.use(cors()); // Support for CROSS-ORIGN
+
+
 
 // App listening
 app.listen(8888);
