a4ececa9c96fe2622131740020fc7a74b19cdd22
Mederic D <m.dulondel@gmail.com>
2015-12-23 10:17:25 +0100

    JS Beautify

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,18 @@
 var app = angular.module('bclub', ['ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
 
-  $routeProvider
-    .when('/home', {
-      controller: 'HomeController',
-      templateUrl: 'templates/accueil.html'
-    })
-    .when('/chemin', {
-      templateUrl: 'templates/chemin.html',
-      controller: 'CheminController'
-    })
-    .otherwise({
-      redirectTo: '/home'
-    });
+    $routeProvider
+        .when('/home', {
+            controller: 'HomeController',
+            templateUrl: 'templates/accueil.html'
+        })
+        .when('/chemin', {
+            templateUrl: 'templates/chemin.html',
+            controller: 'CheminController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
 
 }]);
