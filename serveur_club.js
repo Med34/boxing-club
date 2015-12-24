90ee7e2c6cf63b76b72edc43eb75f77aaa9a5e9c
Mederic DULONDEL <m.dulondel@gmail.com>
2015-12-24 13:08:27 +0100

    Function map

--- serveur_club.js
+++ serveur_club.js
@@ -1,20 +1,38 @@
-// Set the configuration
+// Configuration
 var express = require("express");
 var fs = require("fs");
 var app = express();
 
-//Cherche toutes les propriétés du document et toutes les valeurs possibles.
+// Cherche toutes les clés du document et toutes les valeurs possibles.
 function map(json) {
-    //return json;
+    var values = {};
+    for (var i = 0; i < json.length; i++) {
+        var properties = Object.keys(json[i]);
+        for (var prop = 0; prop < properties.length; prop++) {
+            var indexProperties = properties[prop];
+            var valueProperties = json[i][indexProperties];
+            // Création de l'index.
+            if (values[indexProperties] === undefined) {
+                values[indexProperties] = [valueProperties];
+            } else {
+                // Ajout d'une valeur à l'attribut s'il n'existe pas.
+                if (values[indexProperties].indexOf(valueProperties) == -1) {
+                    values[indexProperties].push(valueProperties);
+                }
+            }
+        }
+    }
+
+    return JSON.stringify(values);
 }
 
 app.get("/listeMembres", function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/json");
-    var chaineListeMembres = fs.readFileSync("public/datas/boxing_clubs.json", "UTF-8");
+    var chaineListeMembres = fs.readFileSync("public/datas/boxers.min.json", "UTF-8");
     var listeMembres = JSON.parse(chaineListeMembres);
-    console.log(listeMembres);
-    //map(listeMembres);
+    var json = map(listeMembres);
+    res.end(json);
 });
 
 // App listening
