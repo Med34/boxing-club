8f6c100c02dcd4ea6b51c18a084162328cb593f4
Médéric D <m.dulondel@gmail.com>
2016-01-02 11:39:03 +0100

    Rename angular app folder

---
+++ bclubModule/js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('bclub', ['ngRoute']);
+
+app.run(function ($rootScope, $location, $routeParams) {});
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            controller: 'HomeController',
+            templateUrl: 'templates/home.html'
+        })
+        .when('/findMember', {
+            controller: 'FindMemberController',
+            templateUrl: 'templates/findMember.html'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
---
+++ bclubModule/js/controllers.js
@@ -0,0 +1,23 @@
+angular.module('bclub').controller("HomeController", function ($scope) {
+    console.log("HomeController");
+});
+
+angular.module('bclub').controller("FindMemberController", function ($scope, $http) {
+    $http.get("http://localhost:8888/findMember").then(function (response) {
+        $scope.mapJSON = response.data;
+    });
+
+    // Stocke l'utilisateur.
+    $scope.user = {};
+    $scope.find = function (user) {
+        $http.get("http://localhost:8888/listMembers", {
+            params: user
+        }).then(function (response) {
+            $scope.members = response.data;
+        });
+    }
+    $scope.reset = function () {
+        $scope.user = {};
+        $scope.members = null;
+    }
+});
